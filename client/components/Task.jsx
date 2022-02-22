de9cd222f74c3ef45ca31c164ff010ffb56ba4be
andreas <benzan81@gmail.com>
2022-02-22 16:35:45 +1300

    inline editing task title

--- client/components/Task.jsx
+++ client/components/Task.jsx
@@ -7,6 +7,11 @@ function Task (props) {
   const taskGS = useSelector(globalState => globalState.taskReducer.find(task => task.t_id === Number(taskProp.t_id)))
   const dispatch = useDispatch()
 
+  //Inline editing:
+  const [isEditing, setEditing] = useState(false);
+  const [TaskTitle, setTaskTitle] = useState(taskProp.title)
+
+
   const handleChange = (e) => {
   taskGS.status === "completed" ? dispatch(updateATaskInDB({...taskGS, status: "in progress"})) : dispatch(updateATaskInDB({...taskGS, status: "completed"}))
   };
@@ -16,12 +21,46 @@ function Task (props) {
     dispatch(deleteATaskInDB(taskProp.t_id))
   }
 
+  const handleKeyDownTaskTitle = (e) => {
+    setTaskTitle(e.target.value)
+  };
+
+const handleUpdate = (e) => {
+  setEditing(false)
+  dispatch(updateATaskInDB({...taskGS, title: TaskTitle}))
+}
 return (
     <>
-      <li className={(taskGS.status === "completed") ? 'completed' : 'in progress'}>
+      <li className={(taskGS.status === "completed") ? 'completed' : ""}>
         <div className="view">
           <input className="toggle" type="checkbox" checked={(taskGS.status === "completed") ? true : false} onChange={handleChange}/>
-          <label>{taskProp.title}</label>
+
+          <label>
+            {isEditing ? (
+              <div
+                onBlur={() => handleUpdate()}
+                onKeyDown={e => handleKeyDownTaskTitle(e)}
+                
+              >
+                <input
+                  type="text"
+                  name="pageTitle"
+                  className="editing"
+                  placeholder={TaskTitle}
+                  value={TaskTitle}
+                  onChange={e => setTaskTitle(e.target.value)}
+                />
+
+              </div>
+            ) : (
+              <div
+                onClick={() => setEditing(true)}
+              >
+                {TaskTitle}
+              </div>
+            )}
+          </label>
+              
           <button className="destroy" onClick={handleDelete}></button>
         </div>
         <input className="edit" readOnly="Create a TodoMVC template"  />
